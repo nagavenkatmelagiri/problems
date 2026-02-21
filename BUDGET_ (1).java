0f7d460c7e482673614845ff9304d3ca1bfe9b1a
nagavenkatmelagiri <nagavenkat205@gmail.com>
2026-02-21 12:49:11 +0530

    first commit

---
+++ BUDGET_ (1).java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes he
+	Scanner sc = new Scanner(System.in);
+	int t = sc.nextInt();
+	while(t-->0){
+	   int a = sc.nextInt();
+	   int b = sc.nextInt();
+	   int days = 30;
+	   int exp = days * b;
+	   if(exp < a){
+	       System.out.println("YES");
+	   }
+	   else{
+	  System.out.println("NO");
+	}
+
+	}
+	    
+	}
+}
